e8e8727b4127203df99cbaf3c26692260cc0ba8c
Bingyu Li USTC <124509769+LiBingyu01@users.noreply.github.com>
2026-05-25 17:37:10 +0800

    Delete GSVA-bi_fusion directory

--- GSVA-bi_fusion/rm.sh
+++
@@ -1,3 +0,0 @@
-rm -r /gemini/space/zhaozy/libingyu/GSVA-main/run_7B/GSVA_bi_fusion_7B_NS_RS.bin
-rm -r /gemini/space/zhaozy/libingyu/GSVA-main/run_7B/GSVA_bi_fusion_7B_RS_NS.bin
-rm -r /gemini/space/zhaozy/libingyu/GSVA-main/run_7B/GSVA_bi_fusion_7B_train_val.bin
